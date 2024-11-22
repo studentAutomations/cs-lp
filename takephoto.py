7ff592aec1b5d7feaea5522230c91277c8846e56
studentAutomations <student.automations@gmail.com>
2024-11-22 16:17:22 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -44,17 +44,22 @@ try:
     time.sleep(5)
 
     page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
-    time.sleep(30)
+    time.sleep(5)
 
 
     responseT = page_to_scrape.find_element(By.CLASS_NAME, "table.discussion-list")
 
-    # Get the size of the element and set window size accordingly
+    # Get the size of the element
     height = responseT.size['height']
     width = responseT.size['width']
 
     # Set a larger window size (you can adjust these values as needed)
-    page_to_scrape.set_window_size(width + 100, height + 100)  # Adding some extra space
+    # Here we set a fixed width that is larger than what might be needed.
+    desired_width = max(width + 100, 1200)  # Ensure at least 1200px width
+    page_to_scrape.set_window_size(desired_width, height + 100)  # Adding some extra space
+
+    # Scroll to make sure the element is visible in case it's off-screen
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
 
     # Take a screenshot of the entire element
     responseT.screenshot('cs-lp-nova-obavestenja.png')
