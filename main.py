38bc6271aba9544deb4deb8694738147d39ad2ee
studentAutomations <student.automations@gmail.com>
2024-11-22 22:45:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,7 @@ try:
     page_to_scrape.find_element(By.LINK_TEXT, "LP").click()
     time.sleep(5)
 
-    link_element = page_to_scrape.find_element(By.XPATH, "//a[@https://cs.elfak.ni.ac.rs/nastava/mod/forum/view.php?id=879']")
-    link_element.click()
+    page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
     time.sleep(5)
 
 
