1de2c6d1da0bba37c603a5ae138d6e1e5395a407
studentAutomations <student.automations@gmail.com>
2024-11-25 23:24:10 +0100

    Update removephoto.py

--- removephoto.py
+++ removephoto.py
@@ -1,8 +1,7 @@
 import os
 
-# Specify the path to the photo you want to delete
-photo_path = 'https://github.com/studentAutomations/cs-lp/blob/main/cs-lp-nova-obavestenja.png'  # Change this to the actual path of your photo
+photo_path = 'https://github.com/studentAutomations/cs-lp/blob/main/cs-lp-nova-obavestenja.png'  
 
-# Check if the file exists before attempting to delete it
+
 if os.path.exists(photo_path):
-    os.remove(photo_path)  # Delete the file
+    os.remove(photo_path)  
